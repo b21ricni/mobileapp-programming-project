f49ecc601c406768890fef0912369e50f3b08e8f
b21ricni <b21ricni@student.his.se>
2022-05-21 17:04:34 +0200

    Issue not fixed

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -7,13 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         Log.d("==>", "About created");
-
     }
 }
