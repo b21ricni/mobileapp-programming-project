4a75bd54dd23cde87134131c34326a4a2caa768e
b21ricni <b21ricni@student.his.se>
2022-05-21 17:20:43 +0200

    added click for handler back to main Fixed text and layout on about page

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -2,15 +2,30 @@ package com.example.project;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private Button backToMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         Log.d("==>", "About created");
+
+        backToMain = findViewById(R.id.backtomain);
+        backToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "End About Us: ");
+                finish();
+            }
+        });
+
+
     }
 }
